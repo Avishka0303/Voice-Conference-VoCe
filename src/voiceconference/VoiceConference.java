06672121922eb370ea95273ea81d928d621853e9
Avishka Dhanajaya <avishka0303@gmail.com>
2019-09-25 20:37:17 +0530

    Change the main

--- src/voiceconference/VoiceConference.java
+++ src/voiceconference/VoiceConference.java
@@ -28,9 +28,6 @@ public class VoiceConference extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         guiLaunch = new GUILaunch();
         launch(args);
