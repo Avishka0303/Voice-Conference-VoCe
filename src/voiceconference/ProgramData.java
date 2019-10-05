ae391251fdde0c3fe8989756eacfa772b0e470c2
Avishka Dhanajaya <avishka0303@gmail.com>
2019-10-06 02:00:11 +0530

    Program data updated with multicasting details

--- src/voiceconference/ProgramData.java
+++ src/voiceconference/ProgramData.java
@@ -3,4 +3,6 @@ package voiceconference;
 public interface ProgramData {
     public static final int PACKET_SIZE = 256;
     public static final int PORT_NUMBER = 2000;
+    public static final int MUL_PORT_NUMBER = 2001;
+    public static final String MULTICAST_ADDRESS = "230.0.0.1";     // CLASS D - MULTICAST ADDRESS
 }
